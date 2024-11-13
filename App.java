d31cc2e83a0eda4c76d6d812e898db614ca76c84
Tejesh95 <n210594@rguktn.ac.in>
2024-11-13 23:48:36 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        // frame.setVisible(true);
+		frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon bird = new ImageIcon("flappybird.png");
+        frame.setIconImage(bird.getImage());
+
+
+        Flappybird flappyBird = new Flappybird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+    }
+}
